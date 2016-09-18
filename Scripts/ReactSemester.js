f86c3b0ef5d7bbc594afedae71d7eaed7f8a0066
jignateous <jgn@iastate.edu>
2016-09-18 16:05:59 -0500

    Updated

--- Scripts/ReactSemester.js
+++ Scripts/ReactSemester.js
@@ -10,7 +10,7 @@ var SemesterBox = React.createClass({
     render: function() {
         return (
             <div className="semesterBox">
-                <h3>Fall Semester</h3>
+                <h3 id="fSemester">Fall Semester</h3>
                 <ClassList class="card" data={this.props.data} />
 
             </div>
@@ -22,7 +22,7 @@ var ClassList = React.createClass({
     render: function() {
         var classNodes = this.props.data.map(function(classes) {
             return (
-                <Class classes={classes.myclass} key={classes.id}>
+                <Class classesName={classes.myclass} key={classes.id}>
                     {classes.myclass}
                     {classes.semester}
 
@@ -38,20 +38,23 @@ var ClassList = React.createClass({
 });
 
 var Class = React.createClass({
-    rawMarkup: function() {
-        var md = new Remarkable();
-        var rawMarkup = md.render(
-            this.props.children.toString()
-        );
-        return { __html: rawMarkup };
-    },
     render: function() {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
         return (
             <div className="card-panel hoverable" id="classCard">
                 <div id="myClass">
                     {this.props.myclass}
-                    </div>
-                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+                </div>
+                <div className="card-content white-text">
+                    <span className="card-title"></span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div className="card-action">
+                    <a href="#">{this.props.semester}</a>
+                    <a href="#">This is a link</a>
+                </div>
             </div>
         );
     }
